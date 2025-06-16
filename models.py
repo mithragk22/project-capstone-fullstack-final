223baf01f142520c85146fcf024e50002978b717
Mithragk22 <123443028+mithragk22@users.noreply.github.com>
2025-06-16 09:18:31 +0530

    Added new tables

--- models.py
+++ models.py
@@ -1,13 +1,15 @@
 import os
-from sqlalchemy import Column, String, create_engine
+from sqlalchemy import Column, String, Integer, create_engine, ForeignKey
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 import json
 
 database_path = os.environ['DATABASE_URL']
 if database_path.startswith("postgres://"):
   database_path = database_path.replace("postgres://", "postgresql://", 1)
 
-print(f"Database URL: {database_path}")  
+print(f"Database URL test: {database_path}")  
+print(f"Hi there! Excited: {os.environ.get('EXCITED', 'false')}")
 
 db = SQLAlchemy()
 
@@ -44,9 +46,86 @@ class Person(db.Model):
       'name': self.name,
       'catchphrase': self.catchphrase}
   
+  """
+Creating Movie Table
+
+"""
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(String)
+    actors = relationship('Actor', backref="movie", lazy=True)
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    #def __repr__(self):
+    #    return f'<movies: id: {self.id.data}'
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date,
+            'actors': list(map(lambda actor: actor.format(), self.actors))
+        }
+
+"""
+Creating Actor table
+
+"""
+class Actor(db.Model):
+    __tablename__ = 'actors'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    gender = Column(String)
+    movie_id = Column(Integer, ForeignKey('movies.id'), nullable=True)
+
+    def __init__(self, name,age,gender,movie_id):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.movie_id = movie_id
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender':self.gender,
+            'movie_id':self.movie_id
+          }
+  
   # FLASK-SETUP This code should be at the bottom of all your files.
 if __name__ == '__main__':
-    #with app.app_context():
-       # db.create_all()  # This will create all tables defined by your models
+    with app.app_context():
+      db.create_all()  # This will create all tables defined by your models
     app.debug = True
     app.run(host='0.0.0.0', port=3000)

13227a0b1876c86a59310c29534ce28b3da0f803
Mithragk22 <123443028+mithragk22@users.noreply.github.com>
2025-06-19 20:14:48 +0530

    Update models.py

--- models.py
+++ models.py
@@ -8,9 +8,7 @@ database_path = os.environ['DATABASE_URL']
 if database_path.startswith("postgres://"):
   database_path = database_path.replace("postgres://", "postgresql://", 1)
 
-print(f"Database URL test: {database_path}")  
-print(f"Hi there! Excited: {os.environ.get('EXCITED', 'false')}")
-
+# print(f"Database URL test: {database_path}") 
 db = SQLAlchemy()
 
 '''
@@ -22,31 +20,28 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
+
+    '''
+db_drop_and_create_all()
+    drops the database tables and starts fresh
+    can be used to initialize a clean database
+    !!NOTE you can change the database_filename variable to have multiple verisons of a database
+'''
+
+
+def db_drop_and_create_all():
+    db.drop_all()
     db.create_all()
 
+    # add one demo row which is helping in POSTMAN test
+    movie = Movie(title='Movie1', release_date="jan")
+    movie.insert()
+    print(movie)
 
-'''
-Person
-Have title and release year
-'''
-class Person(db.Model):  
-  __tablename__ = 'People'
-
-  id = Column(db.Integer, primary_key=True)
-  name = Column(String)
-  catchphrase = Column(String)
-
-  def __init__(self, name, catchphrase=""):
-    self.name = name
-    self.catchphrase = catchphrase
-
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'catchphrase': self.catchphrase}
+    actor = Actor(name='actor1', age=25, gender='Female', movie_id=1)
+    actor.insert()
   
-  """
+"""
 Creating Movie Table
 
 """
@@ -121,11 +116,5 @@ class Actor(db.Model):
             'age': self.age,
             'gender':self.gender,
             'movie_id':self.movie_id
-          }
-  
-  # FLASK-SETUP This code should be at the bottom of all your files.
-if __name__ == '__main__':
-    with app.app_context():
-      db.create_all()  # This will create all tables defined by your models
-    app.debug = True
-    app.run(host='0.0.0.0', port=3000)
+          }  
+

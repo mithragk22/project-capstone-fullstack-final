cad11193e52868b2fbe125995ff85a3a83858326
Mithragk22 <123443028+mithragk22@users.noreply.github.com>
2025-06-20 13:17:12 +0530

    addresssed review comments

--- auth.py
+++ auth.py
@@ -1,3 +1,4 @@
+import os
 import json
 from flask import request, _request_ctx_stack
 from functools import wraps
@@ -5,9 +6,9 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'dev-eezq7zycigop1hot.us.auth0.com'
+AUTH0_DOMAIN = os.environ['AUTH0_DOMAIN']
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'castingAgency'
+API_AUDIENCE = os.environ['API_AUDIENCE']
 
 ## AuthError Exception
 '''
